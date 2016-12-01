e0901b9729b877e86038066aacf59ce2335b3da4
sarahshy <sarahshy@Sarahs-MacBook-Air.local>
2016-11-30 23:21:22 -0500

    11/30 Backup

--- A6_script.js
+++ A6_script.js
@@ -2,5 +2,29 @@
 // zoomooz.js can transition to next item while staying zoomed
 // other animations for individual objects
 
+function enterValue() {
+    console.log("Value entered!")
+    var value = $(':input').val();
+    console.log("Value set");
 
-$.fn.scrollPath("getPath");
+    hideTask();
+    console.log("Task hidden");
+
+    if ($(':input').val() == 0.05) { ######neeed number not text??
+        console.log("fail to reject the null");
+    }
+};
+
+
+function hideTask() {
+    $('#input').remove();
+    $('#gameInst').remove();
+};
+
+function showHooray() {
+
+};
+
+function showBoo() {
+
+};
