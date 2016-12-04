d8e69c6ca5ee6dd87529f67d735f6e0268eee378
sarahshy <sarahshy@Sarahs-MacBook-Air.local>
2016-12-03 21:49:40 -0500

    12/3 Backup #1

--- A6_script.js
+++ A6_script.js
@@ -2,29 +2,78 @@
 // zoomooz.js can transition to next item while staying zoomed
 // other animations for individual objects
 
+// $('#images').hover(function() {
+//     console.log("hovering!");
+//     $('#hobbies').css({"height": "40px", "width": "40px", "border-radius": "20px", "margin": "25px", "-webkit-transition": "all 0.3s ease-out", "-moz-transition": "all 0.3s ease-out", "-ms-transition": "all 0.3s ease-out", "-o-transition": "all 0.3s ease-out", "transition": "all 0.3s ease-out"});
+
+// }, function() {
+//         console.log("unhovering!");
+//         $('#hobbies').css({"height": "90px", "width": "90px", "border-radius": "45px", "-webkit-transition": "all 0.3s ease-out", "-moz-transition": "all 0.3s ease-out", "-ms-transition": "all 0.3s ease-out", "-o-transition": "all 0.3s ease-out", "transition": "all 0.3s ease-out"});
+//     }
+
+// );
+
+
 function enterValue() {
     console.log("Value entered!")
     var value = $(':input').val();
     console.log("Value set");
+    $('#gameInst').fadeOut();
+    $('#falseMessage').fadeOut();
 
-    hideTask();
-    console.log("Task hidden");
+    if ((value >= 0) && (value <= 1)) {
+        if (value > 0.05) {
+            console.log("fail to reject the null");
+            console.log(value);
+            $('#contactme').css("background-color", "#8C9F9E")
+            showHooray();
+        }
 
-    if ($(':input').val() == 0.05) { ######neeed number not text??
-        console.log("fail to reject the null");
+        else {
+            console.log("reject the null");
+            console.log(value);
+            $('#contactme').css("background-color", "#8C9F9E")
+            showBoo();
+        }
+
+        hideTask();
+        console.log("Task hidden");
+        showHypotheses();
+        $('#contTitle').fadeOut();
+    }
+
+    else {
+        $(':input').val("");
+        showFalseMessage();
     }
 };
 
 
 function hideTask() {
-    $('#input').remove();
-    $('#gameInst').remove();
+    $('#input').fadeOut();
+    $('#gameInst').fadeOut();
 };
 
 function showHooray() {
+    var value = $(':input').val();
+    $('#hooray').fadeIn();
+    $('.insertValue').text(value);
 
 };
 
 function showBoo() {
-
+    var value = $(':input').val();
+    $('#boo').fadeIn();
+    $('.insertValue').text(value);
 };
+
+function showHypotheses() {
+    $('#hypotheses').fadeIn();
+}
+
+function showFalseMessage() {
+    $('#falseMessage').fadeIn();
+    $('#contactme').css("background-color", "red")
+};
+
+
